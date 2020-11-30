c198ad98e5d73b87e351633331d7500b9447ad1a
Saisushmithavk <46071629+Saisushmithavk@users.noreply.github.com>
2020-12-01 00:36:58 +0530

    admin features added

--- services/admin.js
+++ services/admin.js
@@ -3,7 +3,7 @@ var email = document.getElementById("email").value;
 var password = document.getElementById("password").value;
 if ( email == "admin@gmail.com" && password == "admin#123"){
 alert ("Login successfully");
-window.location.href="userbookings.html"; // Redirecting to other page.
+window.location.href="addpackages.html"; // Redirecting to other page.
 localStorage.setItem("ADMIN_LOGGED_IN",JSON.stringify(obj));
 localStorage.getItem("ADMIN_LOGGED_IN");
 return false;
